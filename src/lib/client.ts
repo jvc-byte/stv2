14dcb81888a13b662598fe85449ac4ad47b688d3
jvc <jvc8463@gmail.com>
2025-02-08 02:26:29 +0100

    modified client.ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,9 +1,20 @@
 import { createThirdwebClient } from "thirdweb";
 
-const clientID = process.env.NEXT_PUBLIC_CLIENT_ID;
+const secretKey = process.env.THIRDWEB_SECRET_KEY;
+const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!;
 
-if (!clientID) {
-  throw new Error("NEXT_PUBLIC_CLIENT_ID is not set");
+if (!clientId) {
+  throw new Error("NEXT_PUBLIC_THIRDWEB_CLIENT_ID is not set");
 }
 
-export const client = createThirdwebClient({clientId: clientID});
+if (!secretKey) {
+  throw new Error("THIRDWEB_SECRET_KEY is not set");
+}
+
+export const client = createThirdwebClient(
+  secretKey
+    ? { secretKey }
+    : {
+        clientId,
+      }
+);
