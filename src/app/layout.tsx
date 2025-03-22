3313bd3c74ae72aa0488a9f826d804e7d3470de4
jvc <jvc8463@gmail.com>
2025-03-22 04:13:45 +0100

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThirdwebProvider>
           {children}
+          <Analytics />
         </ThirdwebProvider>
       </body>
     </html>
